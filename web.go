a8a9ef641129d4373db0ea788fff32b36d449dcf
Danilo Braband <d.braband@k-newmedia.de>
2013-04-24 14:24:32 +0200

    Get Favicon from google service.

--- web.go
+++ web.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -15,7 +17,7 @@ func main() {
 		port = "5000"
 	}
 
-	fmt.Println("listening on port " + port)
+	log.Println("listening on port " + port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		panic(err)
 	}
@@ -32,10 +34,37 @@ Serves the favicon from the following sources:
 5. Default Icon
 */
 
-func index(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "/favicon?domain=google.com")
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "/favicon?domain=google.com")
 }
 
-func favicon(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, req.FormValue("domain"))
+func favicon(w http.ResponseWriter, r *http.Request) {
+	icon, err := fromGoogle(r.FormValue("domain"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Cache-Control", "public, max-age=86400")
+
+	fmt.Fprintf(w, "%s", icon)
+}
+
+func fromGoogle(domain string) ([]byte, error) {
+	log.Println(fmt.Sprintf("Fetch from Google for domain '%s'", domain))
+
+	service := fmt.Sprintf("https://plus.google.com/_/favicon?domain=%s", domain)
+	response, err := http.Get(service)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return contents, nil
 }
